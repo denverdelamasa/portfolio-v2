8f730d55b3c71b2b655a2c092aadaef0a5bd7f49
wyane <denver.delamasa@gmail.com>
2025-09-06 03:21:51 +0800

    fix: img issues in phone screens

--- src/components/homepage/content/cards/AdvancementCard.tsx
+++ src/components/homepage/content/cards/AdvancementCard.tsx
@@ -16,14 +16,14 @@ const AdvancementCard: React.FC<AdvancementCardProps> = ({
 }) => {
   return (
     <div className="card md:card-side hover:backdrop-blur-[2px] transition-all duration-300 ease-in-out hover:shadow-xl rounded-xl">
-      <figure className="m-8 md:w-62 md:h-32 justify-start overflow-hidden w-1/5">
+      <figure className="m-8 md:w-62 md:h-32 justify-start overflow-hidden md:w-1/5">
         <Image
           src={imageUrl}
           alt={title}
           className="rounded-lg object-fill"
         />
       </figure>
-      <div className="card-body w-4/5">
+      <div className="card-body md:w-4/5">
         <h2 className="card-title text-3xl">{title}</h2>
         <p className="text-xs text-base-content/40 max-w-prose">{dateStarted}</p>
         <p className="text-sm text-base-content/80 max-w-prose">{description}</p>
