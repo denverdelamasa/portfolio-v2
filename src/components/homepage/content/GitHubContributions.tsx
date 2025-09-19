dc9c407daf8069076fd10d03a2824fca056f936b
wane <denver.delamasa@gmail.com>
2025-09-20 01:21:59 +0800

    style: edited min height at contributions

--- src/components/homepage/content/GitHubContributions.tsx
+++ src/components/homepage/content/GitHubContributions.tsx
@@ -3,7 +3,7 @@ import ContributionGraph from "./cards/ContributionGraph";
 
 export default function GitHubContributions() {
   return (
-    <section id="github-contributions" className="hero flex flex-col text-3xl w-full min-h-[100vh] items-start px-4">
+    <section id="github-contributions" className="hero flex flex-col text-3xl w-full h-auto items-start px-4">
       <div className="hero-content flex flex-col mb-4 items-start text-start w-full h-full">
         <div className="divider mb-2 mt-12">
             <p className="text-xl font-bold items-center align-middle flex flex-row gap-x-2">
