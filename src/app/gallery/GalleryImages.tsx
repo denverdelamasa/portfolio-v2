391f9462f1d96de035ac0b5d882ab0555bf331ab
wyane-linux <denver.delamasa@gmail.com>
2025-09-26 00:24:10 +0800

    feat: added GalleryImages.tsx

---
+++ src/app/gallery/GalleryImages.tsx
@@ -0,0 +1,5 @@
+export default function GalleryImages() {
+    return (
+        <h1 className="text-8xl font-bold">Images will load here</h1>
+    );
+}
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,9 @@
+import GalleryImages from "./GalleryImages";
+
 export default function Gallery() {
   return (
     <main className="min-h-[100vh] flex flex-col items-center justify-center p-24">
-      <h1 className="text-8xl font-bold">Gallery page</h1>
-      <p className="text-xl mt-4 text-gray-600">
-        Coming soon! Edit <code>page.tsx</code> to change it.
-      </p>
+      <GalleryImages/>
     </main>
   );
 }
