e8d34cbb0b150683cd5100722e9ccb2a71bc58c9
wyane-ubuntu <denver.delamasa@gmail.com>
2025-09-29 01:11:09 +0800

    style: enlarged typography in homepage hero

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -4,17 +4,17 @@ export default function Hero() {
   return (
     <div className="hero h-full w-full">
       <div className="hero-content text-center">
-        <div className="max-w-3xl text-center items-center justify-center align-middle z-0">
-          <p className="text-lg">
+        <div className="max-w-8xl text-center items-center justify-center align-middle z-0">
+          <p className="text-2xl">
             Hi! I&apos;m
           </p>
-          <h1 className="text-7xl lg:text-8xl font-bold mb-2 hero-title">
+          <h1 className="text-8xl lg:text-9xl font-bold hero-title">
             Denver <br className="visible md:hidden"/> Dela Masa
           </h1>
-          <h2 className="text-lg lg:text-2xl font-semibold mb-2">
+          <h2 className="text-xl lg:text-4xl font-bold mb-4">
             Web Developer. Illustrator. Tech Support.
           </h2>
-          <p className="text-xs lg:text-md mx-auto mb-4">
+          <p className="text-xs lg:text-lg mx-auto mb-4 max-w-xl">
             I build responsive websites, make character illustrations, and solve tech problems.
           </p>
           <div className="grid grid-cols-2 md:grid-cols-1 gap-2 w-auto mx-auto">
