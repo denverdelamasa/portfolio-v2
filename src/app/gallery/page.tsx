989b72ee95e9df124002ba94b82209b606ed7a32
wyane <denver.delamasa@gmail.com>
2025-09-28 09:26:18 +0800

    style: font decreased in gallery page.tsx
    I meant to say gallery, not contact page... in last commit

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -7,7 +7,7 @@ export default function Gallery() {
   return (
     <main className="min-h-[100vh] flex flex-col items-center justify-center lg:p-12">
       <h1 className="text-6xl md:text-8xl font-bold">My Gallery</h1>
-      <p className="text-xl mt-4 text-gray-600 max-w-prose text-center">
+      <p className="text-sm md:text-xl mt-4 text-gray-600 max-w-prose text-center">
         Here&apos;s a collection of photos that capture my work, hobbies, and passions. No particular order, just moments that represent what I enjoy.
       </p>
       <GalleryImages images={images} />
