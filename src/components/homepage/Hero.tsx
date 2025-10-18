c7f1433a8050d2a4b84a2ee385c3e06930f910d3
wyane <denver.delamasa@gmail.com>
2025-10-18 14:33:23 +0800

    style: font-size decreased in Hero.tsx

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
               Tech Support
             </span>.
           </h2>
-          <p className="text-xs lg:text-lg mx-auto mb-4 max-w-xl">
+          <p className="text-xs lg:text-md mx-auto mb-4 max-w-xl">
             I build responsive websites, draw character illustrations, and solve tech problems. I aspire to become better at what I do, so I always do my best to learn as best as I could. 
           </p>
           <div className="flex flex-wrap gap-2 w-full mx-auto">
