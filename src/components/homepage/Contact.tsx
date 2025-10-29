ea0e41f69c83c68e8f71df7d0173e6d402b6a3ea
wyane-ubuntu <denver.delamasa@gmail.com>
2025-10-29 23:04:03 +0800

    style: hover animation tweaks

--- src/components/homepage/Contact.tsx
+++ src/components/homepage/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
       <div className="hero-content text-center flex flex-col md:flex-row gap-10 w-full h-fit">
         <div className="w-full md:w-1/2 text-left items-left justify-left align-middle">
           <h1 className="mt-24 lg:mt-auto text-7xl md:text-8xl font-bold mb-4">
-            Let&apos;s work <span className="hover:text-red-500 transition-colors duration-200 ease-in">together</span>!
+            Let&apos;s work <span className="hover:text-red-500 font-thin hover:font-extrabold transition-all duration-200 ease-in">together</span>!
           </h1>
           <p className="text-sm lg:text-lg lg:max-w-lg font-thin mb-2">
             Interested in working together? I&apos;m looking for opportunities to join a team and grow—let’s connect!
